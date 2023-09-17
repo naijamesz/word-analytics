167cd150842998e739e0ff0ca809cb60ee602a89
naijamesz <kodlikejamesz@hotmail.com>
2023-09-17 12:30:17 +0700

    Updated

--- script.js
+++ script.js
@@ -42,13 +42,37 @@ const inputHandler = () => {
 }
 
 textareaEl.addEventListener('input', inputHandler)
+
 // textareaEl.addEventListener('input', function () {
+//   //example of in validation
+//   if (textareaEl.value.includes('<script>')) {
+//     alert(`You can't use <script> in your text.`)
+//     textareaEl.value = textareaEl.value.replace('<script>', '')
+//   }
 //   // determine new numberss
+//   const numberOfWords = textareaEl.value.split(' ').length
+//   if (textareaEl.value.length === 0) {
+//     numberOfWords = 0
+//   }
 //   const numberOfCharacters = textareaEl.value.length
 //   const twitterCharactersLeft = 280 - numberOfCharacters
 //   const facebookCharactersLeft = 2200 - numberOfCharacters
 
+//   // add visual indicator if Limit is exceeded
+//   if (twitterCharactersLeft < 0) {
+//     twitterNumberEl.classList.add('stat__number--limit')
+//   } else {
+//     twitterNumberEl.classList.remove('stat__number--limit')
+//   }
+//   // facebook characters indicator if Limeit is exceed
+//   if (facebookCharactersLeft < 0) {
+//     facebookNumberEl.classList.add('stat__number--limit')
+//   } else {
+//     facebookNumberEl.classList.remove('stat__number--limit')
+//   }
+
 //   // set new numberss
+//   wordsNumberEl.textContent = numberOfWords
 //   charactersNumberEl.textContent = numberOfCharacters
 //   twitterNumberEl.textContent = twitterCharactersLeft
 //   facebookNumberEl.textContent = facebookCharactersLeft
